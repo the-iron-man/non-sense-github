13d4259175b797a191f4bc844208461ae7e28c2e
vikaskyadav <vikasharry03@gmail.com>
2018-01-08 03:54:00 +0530

    Nonsense commit by Hack bot

--- run.py
+++ run.py
@@ -5,19 +5,10 @@ import os
 
 n = int(raw_input("HAck COunt: "))
 filename = "abc"
-
-yy = "2018"
-mm = "01"
-commits_in_a_day = n/30
-
-
-for dd in range(0,30):
-# for i in range(0,n):
+for i in range(0,n):
     with open(filename, 'a+') as f:
-        for count in range(0,commits_in_a_day):
-            f.write(str(datetime.now()))
-            os.rename(filename,filename)
-            os.system('git add .')
-            os.system('git commit -m "Nonsense commit by Hack bot"')
-            time.sleep(1)
-        os.system('date +%%y%%m%%d -s %s%s%s'%(yy,mm,dd))
+        f.write(str(datetime.now()))
+        os.rename(filename,filename)
+        os.system('git add .')
+        os.system('git commit -m "Nonsense commit by Hack bot"')
+        time.sleep(1)
