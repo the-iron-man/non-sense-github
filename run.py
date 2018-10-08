47411feacf74350e147aaa07fef7319ab936f426
vikaskyadav <vikasharry03@gmail.com>
2018-10-08 23:58:04 +0530

    removed starter

--- run.py
+++ run.py
@@ -5,8 +5,17 @@ import os
 
 n = int(raw_input("HAck COunt: "))
 filename = "abc"
+
+# yy = "2018"
+# mm = "05"
+# commits_in_a_day = n/30
+
+# for dd in range(0,30):
+
 for i in range(0,n):
     with open(filename, 'a+') as f:
+        # for count in range(0,commits_in_a_day)
+        # os.system('date +%Y%m%d -s "' + yymmdd + "'")
         f.write(str(datetime.now()))
         os.rename(filename,filename)
         os.system('git add .')
